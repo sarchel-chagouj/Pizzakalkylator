d53acf0974d0e613e016d79839e630ae2eba1133
sarchel-chagouj <sarchel.cha2019@gmail.com>
2026-09-25 16:19:48 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+
+namespace Pizzakalkylator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double MOMS = 0.12;
+
+            Console.WriteLine("Hur många pizzor vill du köpa?");
+            int antalPizzor = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Vad kostar en pizza?");
+            double prisPerPizza = Convert.ToDouble(Console.ReadLine());
+
+            double totalsummaUtanMoms = antalPizzor * prisPerPizza;
+            double momsbelopp = totalsummaUtanMoms * MOMS;
+            double totalsummaMedMoms = totalsummaUtanMoms + momsbelopp;
+
+            Console.WriteLine("Totalsumma utan moms: " + totalsummaUtanMoms + " kr");
+            Console.WriteLine("Momsbelopp: " + momsbelopp + " kr");
+            Console.WriteLine("Totalsumma med moms: " + totalsummaMedMoms + " kr");
+        }
+    }
+}
